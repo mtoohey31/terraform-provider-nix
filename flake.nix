4df96ae326058dbb94a98d84237bb8cebae3107c
Matthew Toohey <contact@mtoohey.com>
2023-05-04 21:57:46 -0400

    build: swap gow for watchexec

--- flake.nix
+++ flake.nix
@@ -27,28 +27,11 @@
       };
     };
   } // utils.lib.eachDefaultSystem (system: with import nixpkgs
-    {
-      overlays = [
-        (final: _: {
-          gow = final.buildGoModule rec {
-            pname = "gow";
-            version = builtins.substring 0 7 src.rev;
-            src = final.fetchFromGitHub {
-              owner = "mitranim";
-              repo = pname;
-              rev = "a5bfab26a0e42ee646f0969ac3397e80e5e3b1df";
-              sha256 = "vlIbVoAxeeQ1SB8FmSAfQ35fX6f+/VGZmrPDdA3HTvs=";
-            };
-            vendorSha256 = "o6KltbjmAN2w9LMeS9oozB0qz9tSMYmdDW3CwUNChzA=";
-          };
-        })
-        self.overlays.default
-      ]; inherit system;
-    }; {
+    { overlays = [ self.overlays.default ]; inherit system; }; {
     packages.default = terraform-provider-nix;
 
     devShells.default = mkShell {
-      packages = [ go gopls gow terraform terraform-ls ];
+      packages = [ go gopls terraform terraform-ls watchexec ];
     };
   });
 }
